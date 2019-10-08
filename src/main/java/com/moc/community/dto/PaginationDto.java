8c76fe6b4b7cf12b55328917f3bf617a89339ca1
moc_community <nuc2015@163.com>
2019-10-08 21:41:14 +0800

    extracting fragments in the thymeleaf and fix a bug for pagination

--- src/main/java/com/moc/community/dto/PaginationDto.java
+++ src/main/java/com/moc/community/dto/PaginationDto.java
@@ -41,7 +41,7 @@ public class PaginationDto {
             if (page - i > 0) {
                 pages.add(0, page - i);
             }
-            if (page + i < totalPage) {
+            if (page + i <= totalPage) {
                 pages.add(page + i);
             }
         }
